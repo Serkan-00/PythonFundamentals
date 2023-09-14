2c2f2a6670e7c4c23fbf907bf8c804a9b32edc88
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-09-14 15:58:05 +0100

    string methods

---
+++ StringMethods.py
@@ -0,0 +1,8 @@
+hello = 'HELLO World'
+print(hello.count('o')) 
+
+
+#Capitalize = Capitalizes first letter of string 
+#Upper = Converts to capitals 
+#Lower = Converts to lowercase 
+#Count = Counts speicified numbers 
--- arithmetics.py
+++ arithmetics.py
@@ -4,7 +4,7 @@ result  = (x + y) * 20
 print(result)
 
 num = input('Number: ')
-print(int(num) * 5)
+print(float(num) * 5)
 #The int converts str to a number. 
 #The 'int' can be changed to float which will return the number with a decimal.  
 
