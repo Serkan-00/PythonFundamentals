068a4d309055e2b09098919006957f5f1d288a68
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-09-14 16:02:49 +0100

    string * Int

--- StringMethods.py
+++ StringMethods.py
@@ -1,8 +1,12 @@
 hello = 'HELLO World'
 print(hello.count('o')) 
 
-
 #Capitalize = Capitalizes first letter of string 
 #Upper = Converts to capitals 
 #Lower = Converts to lowercase 
-#Count = Counts speicified numbers 
+#Count = Counts speicified numbers
+
+x = 'hello'
+y = 3 
+print (x * y)
+#strings can be multiplied with integers  
