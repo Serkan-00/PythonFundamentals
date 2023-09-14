884d910d6b97bf6d47f404f45d1499582ae2dcf2
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-09-14 15:18:19 +0100

    numbers

---
+++ arithmetics.py
@@ -0,0 +1,10 @@
+x = 10 
+y = 20 
+result  = (x + y) * 20  
+print(result)
+
+num = input('Number: ')
+print(int(num) * 5)
+#The int converts str to a number. 
+#The 'int' can be changed to float which will return the number with a decimal.  
+
